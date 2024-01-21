3b6d08a7bb768eafe5852fbf9239c68845f1a742
Alex Sherman <asherman.ca@gmail.com>
2024-01-21 11:58:49 -0800

    settings page - signout button

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import { auth } from '@/auth'
+import { signOut, auth } from '@/auth'
 
 const page = async () => {
 	const session = await auth()
 
 	console.log('sesh', session)
 
-	return <div>{JSON.stringify(session)}</div>
+	return (
+		<div>
+			{JSON.stringify(session)}
+			<form
+				action={async () => {
+					'use server'
+					await signOut()
+				}}
+			>
+				<button type='submit'>Sign out</button>
+			</form>
+		</div>
+	)
 }
 
 export default page

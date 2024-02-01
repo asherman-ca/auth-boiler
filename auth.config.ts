03ac1c230f610c09acb13eb5ee88b6f57b90933c
Alex Sherman <asherman.ca@gmail.com>
2024-02-01 10:26:59 -0800

    oauth - github and google

--- auth.config.ts
+++ auth.config.ts
@@ -1,13 +1,22 @@
 // workaround to make nextauth's Prisma adapter edge compatible (Prisma doesn't work on the edge runtime)
 import type { NextAuthConfig } from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
-import GitHub from 'next-auth/providers/github'
+import Github from 'next-auth/providers/github'
+import Google from 'next-auth/providers/google'
 import { LoginSchema } from './schemas'
 import { getUserByEmail } from './data/user'
 import bcrypt from 'bcryptjs'
 
 export default {
 	providers: [
+		Google({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+		Github({
+			clientId: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+		}),
 		Credentials({
 			async authorize(credentials) {
 				// this portion of the code doesn't run on the edge so it still works
